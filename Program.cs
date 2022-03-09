269fb82518502c79467da8c3b8579e463ec07dc0
monicaiyb <monicaiyb@gmail.com>
2022-03-09 15:23:04 +0300

    intial commit array_sum

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// namespace declaration
+namespace array_sum
+{
+    // Class declaration
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3 };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            // statement
+            // printing sum
+            Console.WriteLine(sum);
+
+            // To prevents the screen from 
+            // running and closing quickly
+            Console.ReadKey();
+        }
+    }
+}
